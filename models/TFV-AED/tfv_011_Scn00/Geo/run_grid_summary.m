a1bfd50683fb494c8afffe5642e8934008012241
brendan-busch <brendan.busch@uwa.edu.au>
2021-10-08 17:17:48 +0800

    Mapping

---
+++ models/TFV-AED/tfv_011_Scn00/Geo/run_grid_summary.m
@@ -0,0 +1,12 @@
+
+filename = 'Erie_v5_Substrates_NS.2dm';
+
+outfile = 'Erie_v5_Substrates_NS_summary.csv';
+
+summerise_grid(filename,outfile)
+
+filename = 'Erie_V6_Substrates.2dm';
+
+outfile = 'Erie_V6_Substrates_summary.csv';
+
+summerise_grid(filename,outfile)
